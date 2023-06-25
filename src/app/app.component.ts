2ebaa1d2432d7e2292520f2ca220b1b0f08e5ddc
muhammadfandu <muhammad.p.widodo@gmail.com>
2023-06-25 21:18:08 +0700

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpErrorResponse,
+} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-test';
+
+  name = 'Angular';
+  data: any = {};
+  isLoading = false;
+
+  constructor(public httpSvc: HttpClient) { }
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    this.isLoading = true;
+    const getData = this.httpSvc.get('https://pokeapi.co/api/v2/pokemon/1/');
+    getData?.subscribe({
+      next: (res: any) => {
+        this.data = res;
+        this.isLoading = false;
+      },
+      error: (e: HttpErrorResponse) => {
+        this.isLoading = false;
+        console.error(e);
+      },
+    });
+  }
+
+  getPokemonName(pokemon: any) {
+    const name = pokemon.name;
+    return name.toUpperCase();
+  }
 }
